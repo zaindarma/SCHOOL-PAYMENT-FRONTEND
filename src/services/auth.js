0699b19a07a943b6e09f433a7461b742cb47b1c7
Alwi842 <alwi.abdullah1221@gmail.com>
2025-03-20 16:17:33 +0700

    add security

--- src/services/auth.js
+++ src/services/auth.js
@@ -89,7 +89,7 @@ export function isAdminUser() {
 // Handle Logout
 export function logout() {
   localStorage.removeItem("token");
-  window.location.href = "/";
+  window.location.href = "/main";
 }
 
 export async function refreshToken(token) {
